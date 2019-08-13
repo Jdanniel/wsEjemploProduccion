9d131a99e6ba580a53d7f8b97efaa74c485c4ccd
Jdanniel <30804814+Jdanniel@users.noreply.github.com>
2019-08-12 23:20:23 -0500

    Se pasan a mayusculas los estados para mejorar la coincidencia con el cp

--- wsEjemplo/Service1.svc.cs
+++ wsEjemplo/Service1.svc.cs
@@ -580,7 +580,7 @@ namespace wsEjemplo
                        {
                            estatus = b.DESC_STATUS_AR,
                            conclusion = a.DESCRIPCION_TRABAJO,
-                           fechaConcluido = String.Format("{0:dd/MM/yyyy HH:mm:ss}", a.FEC_CIERRE),
+                           fechaConcluido = String.Format("{0:MM/dd/yyyy}", a.FEC_CIERRE),
                            odt = a.NO_AR,
                            motivo = ""
                        }).ToList();
